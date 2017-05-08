61fb6745b9b895e0d108c9a1c47119b6cd941c2d
Танир Фарбазутдинов < >
2017-05-08 12:47:16 +0500

    Initial commit

---
+++ Piligrim.Web/Components/MenuViewComponent.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Piligrim.Web.Models.Menu;
+
+namespace Piligrim.Web.Components
+{
+    public class MenuViewComponent : ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            var model = new MenuViewModel();
+            model.Categories = new[]
+            {
+                new MenuCategory
+                {
+                    Title = "Title1",
+                    MenuItems = new[]
+                    {
+                        new MenuItem {Title = "Menu1", Url = "http://1"},
+                        new MenuItem {Title = "Menu2", Url = "http://1"},
+                    }
+                },
+                new MenuCategory
+                {
+                    Title = "Same"
+                },
+                new MenuCategory
+                {
+                    Title = "Title2",
+                    MenuItems = new[]
+                    {
+                        new MenuItem {Title = "Menu3", Url = "http://1"},
+                        new MenuItem {Title = "Menu4", Url = "http://1"},
+                    }
+                }
+            };
+
+            return this.View(model);
+        }
+    }
+}
---
+++ Piligrim.Web/Models/Menu/MenuViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Piligrim.Web.Models.Menu
+{
+    public class MenuViewModel
+    {
+        public IEnumerable<MenuCategory> Categories { get; set; }
+    }
+
+    public class MenuCategory
+    {
+        public string Title { get; set; }
+
+        public bool HasChild
+        {
+            get { return this.MenuItems != null && this.MenuItems.Any(); }
+        }
+
+        public IEnumerable<MenuItem> MenuItems { get; set; }
+    }
+
+    public class MenuItem
+    {
+        public string Url { get; set; }
+
+        public string Title { get; set; }
+    }
+}
